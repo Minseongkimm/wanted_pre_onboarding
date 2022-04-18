cf23263237d72bd7013ad093f61e626b89b076d5
Minseongkimm <84434605+Minseongkimm@users.noreply.github.com>
2022-04-18 12:51:10 +0900

    toggle 기본기능 구현 완료

---
+++ src/components/Toggle.js
@@ -0,0 +1,46 @@
+import { React, useState } from 'react';
+
+const Toggle = () => {
+  const [toggleState, setToggleState] = useState('기본');
+
+  const detailHandler = () => {
+    setToggleState('상세');
+  };
+
+  const basicHandler = () => {
+    setToggleState('기본');
+  };
+
+  return (
+    <div className="mt-6">
+      <section className="flex ml-2">
+        <div className="h-11 w-32 bg-slate-300 rounded-l-lg  ">
+          <div
+            className={
+              toggleState === '기본'
+                ? 'clickedToggleMenu'
+                : 'unClickedToggleMenu'
+            }
+            onClick={basicHandler}
+          >
+            기본
+          </div>
+        </div>
+        <div className="h-11 w-32 bg-slate-300 rounded-r-lg transition duration-150">
+          <div
+            className={
+              toggleState === '상세'
+                ? 'clickedToggleMenu'
+                : 'unClickedToggleMenu'
+            }
+            onClick={detailHandler}
+          >
+            상세
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Toggle;
