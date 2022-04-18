f9514a941549fb77bd1b50ee64049c171cef0bb7
Minseongkimm <84434605+Minseongkimm@users.noreply.github.com>
2022-04-18 13:45:50 +0900

    Tab 기본기능 구현 완료

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import Tab from './components/Tab';
 import Toggle from './components/Toggle';
 
 function App() {
   return (
     <>
+      <div>1. Toggle</div>
       <Toggle />
+      <div>2. Tab</div>
+      <Tab />
     </>
   );
 }
---
+++ src/components/Tab.js
@@ -0,0 +1,37 @@
+import { React, useState } from 'react';
+
+const Tab = () => {
+  const [id, setId] = useState(0);
+
+  const clickHandler = (id) => {
+    console.log(id);
+    setId(id);
+  };
+
+  return (
+    <div className="mt-6 ml-3">
+      <ul className="flex">
+        <li
+          className={id === 0 ? 'clickedTab' : 'unClickedTab'}
+          onClick={() => clickHandler(0)}
+        >
+          감자
+        </li>
+        <li
+          className={id === 1 ? 'clickedTab' : 'unClickedTab'}
+          onClick={() => clickHandler(1)}
+        >
+          고구마
+        </li>
+        <li
+          className={id === 2 ? 'clickedTab' : 'unClickedTab'}
+          onClick={() => clickHandler(2)}
+        >
+          카레라이스
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Tab;
